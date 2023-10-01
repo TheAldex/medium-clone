60f84d318306257cbc60079fbb85c6c3955b1f38
Alejandro Delgado <ialexx9919@gmail.com>
2023-10-01 13:46:19 +0200

    creating-front-recommendations

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d06e8d7f62f736f4"; }
+/******/ 		__webpack_require__.h = function() { return "5bc296a6c4c16757"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Recommendations.js
+++ components/Recommendations.js
@@ -14,6 +14,23 @@ const styles = {
   searchBar:
     "flex items-center gap-[.6rem] h-[2.6rem] border px-[1rem] rounded-full",
   searchInput: "border-none outline-none bg-none w-full",
+  authorContainer: "my-[2rem]",
+  authorProfileImageContainer: "h-[5rem] w-[5rem] rounded-full overflow-hidden",
+  authorName: "font-semibold mb-[.2rem] mt-[1rem]",
+  authorFollowing: "text-[#787878] ",
+  authorActions: "flex gap-[.6rem] my-[1rem]",
+  actionButton:
+    "bg-[#1A8917] text-white rounded-full px-[.6rem] py-[.4rem] text-sm",
+  recommendationContainer: "",
+  title: "",
+  articlesContainer: "",
+  recommendationAuthorProfileImageContainer:
+    "rounded-full overflow-hidden h-[1.4rem] w-[1.4rem]",
+  recommendationAuthorContainer: "flex items-center gap-[.6rem]",
+  recommendationAuthorName: "text-sm",
+  recommendationTitle: "font-bold",
+  recommendationThumbnailContainer: "",
+  recommendationThumbnail: "",
 };
 
 const Recommendations = () => {
@@ -27,6 +44,44 @@ const Recommendations = () => {
           placeholder="Search"
           type="text"
         />
+      </div>
+      <div className={styles.authorContainer}>
+        <div className={styles.authorProfileImageContainer}>
+          <Image src={Qazi} slt="author-image" width={100} height={100} />
+        </div>
+        <div className={styles.authorName}>Alejandro Delgado</div>
+        <div className={styles.authorFollowing}>1M followers</div>
+        <div className={styles.authorActions}>
+          <button className={styles.actionButton}>Follow</button>
+          <button className={styles.actionButton}>
+            <MdMarkEmailUnread />
+          </button>
+        </div>
+      </div>
+      <div className={styles.recommendationContainer}>
+        <div className={styles.title}>More from Medium</div>
+        <div className={styles.articlesContainer}>
+          <div className={styles.recommendationAuthorContainer}>
+            <div className={styles.recommendationAuthorProfileImageContainer}>
+              <Image src={Qazi} alt="author-image" height={100} width={100} />
+            </div>
+            <div className={styles.recommendationAuthorName}>
+              Alejandro Delgado
+            </div>
+            <div className={styles.recommendationTitle}>
+              The Ultimate Javascript Course for Beginners
+            </div>
+          </div>
+          <div className={styles.recommendationThumbnailContainer}>
+            <Image
+              src={JSLogo}
+              alt="article-image"
+              height={100}
+              width={100}
+              className={styles.recommendationThumbnail}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
